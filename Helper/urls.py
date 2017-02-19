e5d9fab463db04cbde644ec8ad8f90fb8670aa61
Jimbomaniak <jimboman54321@gmail.com>
2017-02-19 21:51:05 +0200

    add url for elements

--- Helper/urls.py
+++ Helper/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.SystemList.as_view(), name='main'),
-    url(r'^helper/(?P<pk>[0-9]+)/$', views.SystemDetail.as_view(), name='element')
+    url(r'system(?P<sys_id>[0-9]+)/$', views.ElementList.as_view(), name='element')
 ]
