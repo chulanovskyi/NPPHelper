df2a26545a1d7ff3d2f95808a1b46f49eac83ea6
Jimbomaniak <jimboman54321@gmail.com>
2017-02-20 18:20:54 +0200

    url with slugs

--- Helper/urls.py
+++ Helper/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.SystemList.as_view(), name='main'),
-    url(r'system(?P<sys_id>[0-9]+)/$', views.ElementList.as_view(), name='element')
+    url(r'(?P<sys_slug>[-\w]+)/$', views.SystemDetail.as_view(), name='system-detail'),
+    url(r'(?P<sys_slug>[-\w]+)/(?P<elem_slug>[-\w]+)/$', views.ElementDetail.as_view(), name='element-detail'),
 ]
+
+#id(?P<elem_id>[0-9]+)
+"""url(r'(?P<sys_slug>[-\w]+)/(?P<elem_slug>[-\w]+)/$',
+views.ElementDetail.as_view(), name = 'element-detail' """
