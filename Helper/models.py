ba74390764dbd8015801eaf0a6f6d6da18738cdb
Jimbomaniak <jimboman54321@gmail.com>
2017-02-20 18:22:21 +0200

    add slugfields

--- Helper/models.py
+++ Helper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 TYPE_CHOICES = (
     ('ЗАДВ', 'Задвижка'),
@@ -19,6 +20,12 @@ class System(models.Model):
     short_name = models.CharField(max_length=8, verbose_name='Короткое обозначение')
     info = models.CharField(max_length=250, null=True, blank=True, verbose_name='Описание системы')
     img = models.ImageField(verbose_name='Принципиальная схема', null=True, blank=True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.short_name).upper()
+        super(System, self).save(*args, **kwargs)
 
     def __str__(self):
         return '{} - {}'.format(self.short_name, self.name)
@@ -35,6 +42,12 @@ class Element(models.Model):
     row = models.CharField(max_length=4, default='А-В', verbose_name='Ряд(ы)')
     axis = models.CharField(max_length=10, default='1-12', verbose_name='Ось(оси)')
     img = models.ImageField(null=True, blank=True,verbose_name='Чертеж,фото,схема')
+    slug = models.SlugField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name).upper()
+        super(Element, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
